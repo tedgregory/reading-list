045c0b5cf101526df4f008ef761cc94b5d38febd
tedgregory <mail@tedgregory.ru>
2025-05-18 15:23:52 +0300

    fix: vercel env vars fix

--- src/lib/fetchBooks.ts
+++ src/lib/fetchBooks.ts
@@ -1,10 +1,12 @@
 import { ApiBook, Book } from '@/types';
 
-const API_LIMIT = parseInt(process.env.NEXT_PUBLIC_API_LIMIT || '10', 10);
-
-export async function fetchBooks(query: string, limit: number | null = API_LIMIT): Promise<Book[]> {
+export async function fetchBooks(query: string, limit: number | null = null): Promise<Book[]> {
+  const API_LIMIT = parseInt(
+    process.env.NEXT_PUBLIC_API_LIMIT || process.env.API_LIMIT || '10',
+    10,
+  );
   const res = await fetch(
-    `https://openlibrary.org/search.json?q=${encodeURIComponent(query)}&limit=${limit}`,
+    `https://openlibrary.org/search.json?q=${encodeURIComponent(query)}&limit=${limit || API_LIMIT}`,
   );
   const data = await res.json();
 
